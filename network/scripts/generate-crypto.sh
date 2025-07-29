603216f63fb2d4ba9baf2faad0e21460c3194f18
wamuumu <matteo68.mc@gmail.com>
2025-07-29 13:03:51 +0200

    Updated generate crypto script

--- network/scripts/generate-crypto.sh
+++ network/scripts/generate-crypto.sh
@@ -2,24 +2,15 @@
 
 . ../network.config
 
-export PATH=${PATH}:${FABRIC_BIN_PATH}
-export FABRIC_CFG_PATH=${FABRIC_CFG_PATH}
+export PATH=${PATH}:${NETWORK_BIN_PATH}
 
 if [ -d ${NETWORK_ORG_PATH} ]; then
     rm -rf ${NETWORK_ORG_PATH}
 fi
-
 mkdir -p ${NETWORK_ORG_PATH}/peerOrganizations ${NETWORK_ORG_PATH}/ordererOrganizations
 
-which cryptogen > /dev/null
-
-if [ $? -ne 0 ]; then
-    echo "cryptogen not found in PATH. Please install Hyperledger Fabric binaries."
-    exit 1
-else
-    echo "Cryptogen tool found at: $(which cryptogen)"
-    cryptogen generate --config=${FABRIC_CFG_PATH}/crypto-config.yaml --output=${NETWORK_ORG_PATH}
-    echo "Cryptographic material generated successfully in ../organizations"
-fi
+echo "Cryptogen tool found at: $(which cryptogen)"
+cryptogen generate --config=${NETWORK_CFG_PATH}/crypto-config.yaml --output=${NETWORK_ORG_PATH}
+echo "Cryptographic material generated successfully in ${NETWORK_ORG_PATH}"
 
 
