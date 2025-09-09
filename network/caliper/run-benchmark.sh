dc48963fe67df08b0012159b21f582fb238907a6
wamuumu <matteo68.mc@gmail.com>
2025-09-09 15:12:50 +0200

    Removed sanity check

--- network/caliper/run-benchmark.sh
+++ network/caliper/run-benchmark.sh
@@ -20,7 +20,7 @@ fi
 BENCHMARK_NAME=$(basename "$BENCHMARK_FILE" .yaml)
 BENCHMARK_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
-(npx caliper launch manager \
+npx caliper launch manager \
     --caliper-flow-only-test \
     --caliper-fabric-gateway-enabled \
     --caliper-workspace ./ \
@@ -29,6 +29,5 @@ BENCHMARK_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
     --caliper-networkconfig networks/network-config-minimal.yaml \
     --caliper-benchconfig $BENCHMARK_FILE \
     --caliper-report-path ./results/${BENCHMARK_NAME}_report_${BENCHMARK_DATE}.html
-); stty sane
 
 echo "Benchmark report generated at ./results/${BENCHMARK_NAME}_report_${BENCHMARK_DATE}.html"
