a6072a995dd23693b57c08b1799d261e4b06eff6
wamuumu <matteo68.mc@gmail.com>
2025-09-10 12:58:00 +0200

    Added caliper benchmarks -wip. Split workloads into two distinct ones (write and read)

---
+++ network/caliper/workloads/tx-workload.js
@@ -0,0 +1,46 @@
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class TxWorkload extends WorkloadModuleBase {
+
+    constructor() {
+        super();
+        this.txIndex = 0;
+    }
+
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+        
+        this.contractName = roundArguments.contractName || "cc-test";
+        this.functionName = roundArguments.functionName || "CreateResource";
+        this.workerIndex = workerIndex;
+    }
+
+    async submitTransaction() {    
+        this.txIndex++;
+        const request = this.createWriteRequest();
+        const tx = await this.sutAdapter.sendRequests(request);
+    }
+
+    createWriteRequest() {
+        const PID = `pid_${this.workerIndex}_${this.txIndex}_${Date.now()}_${Math.random()}`; // Ensure uniqueness
+        const randomHash = Math.random().toString(36).substring(2, 15);
+        const randomTimestamp = (Date.now() + Math.floor(Math.random() * 100000)).toString();
+
+        const request = {
+            contractId: this.contractName,
+            contractFunction: this.functionName,
+            contractArguments: [
+                PID, 
+                "url", 
+                randomHash, 
+                randomTimestamp, 
+                JSON.stringify(["owner1", "owner2"])
+            ],
+            readOnly: false,
+        };
+
+        return request;
+    }
+}
+
+module.exports.createWorkloadModule = () => { return new TxWorkload() };
