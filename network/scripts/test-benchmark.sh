d8006d1a54647176095406b7ec6b3c941753215b
wamuumu <matteo68.mc@gmail.com>
2025-09-15 11:41:24 +0200

    Added third orderer to project for Raft consensus

--- network/scripts/test-benchmark.sh
+++ network/scripts/test-benchmark.sh
@@ -5,6 +5,7 @@
 # ---- For test only ----
 ./docker-down.sh "${NETWORK_CMP_PATH}/docker-compose-ord1.yaml" --hard
 ./docker-down.sh "${NETWORK_CMP_PATH}/docker-compose-ord2.yaml" --hard
+./docker-down.sh "${NETWORK_CMP_PATH}/docker-compose-ord3.yaml" --hard
 ./docker-down.sh "${NETWORK_CMP_PATH}/docker-compose-org1.yaml" --hard
 ./docker-down.sh "${NETWORK_CMP_PATH}/docker-compose-org2.yaml" --hard
 ./docker-down.sh "${NETWORK_CMP_PATH}/docker-compose-org3.yaml" --hard
@@ -18,6 +19,7 @@ mkdir -p ${NETWORK_ORG_PATH} ${NETWORK_CHN_PATH} ${NETWORK_IDS_PATH}
 # Setup the crypto material
 ./network-prep.sh "${NETWORK_CRP_PATH}/crypto-config-ord1.yaml" "${NETWORK_CMP_PATH}/docker-compose-ord1.yaml"
 ./network-prep.sh "${NETWORK_CRP_PATH}/crypto-config-ord2.yaml" "${NETWORK_CMP_PATH}/docker-compose-ord2.yaml"
+./network-prep.sh "${NETWORK_CRP_PATH}/crypto-config-ord3.yaml" "${NETWORK_CMP_PATH}/docker-compose-ord3.yaml"
 ./network-prep.sh "${NETWORK_CRP_PATH}/crypto-config-org1.yaml" "${NETWORK_CMP_PATH}/docker-compose-org1.yaml"
 ./network-prep.sh "${NETWORK_CRP_PATH}/crypto-config-org2.yaml" "${NETWORK_CMP_PATH}/docker-compose-org2.yaml"
 ./network-prep.sh "${NETWORK_CRP_PATH}/crypto-config-org3.yaml" "${NETWORK_CMP_PATH}/docker-compose-org3.yaml"
@@ -29,6 +31,7 @@ mkdir -p ${NETWORK_ORG_PATH} ${NETWORK_CHN_PATH} ${NETWORK_IDS_PATH}
 # Docker setup
 ./docker-up.sh "${NETWORK_CMP_PATH}/docker-compose-ord1.yaml" # Start the orderer 1
 ./docker-up.sh "${NETWORK_CMP_PATH}/docker-compose-ord2.yaml" # Start the orderer 2
+./docker-up.sh "${NETWORK_CMP_PATH}/docker-compose-ord3.yaml" # Start the orderer 3
 ./docker-up.sh "${NETWORK_CMP_PATH}/docker-compose-org1.yaml" # Start the organization 1
 ./docker-up.sh "${NETWORK_CMP_PATH}/docker-compose-org2.yaml" # Start the organization 2
 ./docker-up.sh "${NETWORK_CMP_PATH}/docker-compose-org3.yaml" # Start the organization 3
@@ -36,6 +39,7 @@ mkdir -p ${NETWORK_ORG_PATH} ${NETWORK_CHN_PATH} ${NETWORK_IDS_PATH}
 # Channel setup
 ./network-join-orderer.sh "orderer.ord1.testbed.local"  # TODO: set identity as environment variable
 ./network-join-orderer.sh "orderer.ord2.testbed.local"  # TODO: set identity as environment variable
+./network-join-orderer.sh "orderer.ord3.testbed.local"  # TODO: set identity as environment variable
 ./network-join-organization.sh "org1.testbed.local"     # TODO: set identity as environment variable
 ./network-join-organization.sh "org2.testbed.local"     # TODO: set identity as environment variable
 ./network-join-organization.sh "org3.testbed.local"     # TODO: set identity as environment variable
